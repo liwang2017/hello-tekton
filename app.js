76b4f28ee10b4090cbbd94965709bca6eaea36a5
liwang2017 <liwang@au1.ibm.com>
2020-03-13 17:35:22 +1100

    Fix syntax error

--- app.js
+++ app.js
@@ -13,7 +13,6 @@
 *   See the License for the specific language governing permissions and
 *   limitations under the License.
 **/
-# For test, not commit
 var express = require('express');
 
 var PORT;
